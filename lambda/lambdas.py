1ea0383556e3c02a1d17bba33f76a643b5ee0257
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2023-10-09 11:51:36 +0530

    Lambda functions

---
+++ lambda/lambdas.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+# Lambda declaration
+double_lambda = lambda num: num * 2
+
+multiply = lambda a, b: a * b
+print(multiply(2, 3))
+
+# map(), filter() and reduce()
+
+
+def triple(num):
+    return num * 3
+
+
+my_list = [1, 2, 3]
+double_result = map(lambda num: num * 2, my_list)
+triple_result = map(triple, my_list)
+
+print(list(double_result))
+print(list(triple_result))
+
+filter_result = filter(lambda num: num % 2 == 0, my_list)
+print(list(filter_result))
+
+expenses = [("Dinner", 50), ("Travel", 80)]
+reduce_result = reduce(lambda a, b: a[1]+b[1], expenses)
+print(reduce_result)
