144f085356440b2f49455466dd719ef2069f36a8
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2023-10-15 09:09:04 +0530

    Exceptions and List compression

--- exception_handling/exceptions.py
+++ exception_handling/exceptions.py
@@ -6,3 +6,23 @@ else:
     print("This will execute if there is no error!!!")
 finally:
     print("This will execute whether we have error or not!!!")
+
+# We can raise exceptions from a part of code using raise
+try:
+    raise Exception("This is an exception!!!")
+except Exception as errormessage:
+    print(errormessage)
+
+# Userdefined Exception Class
+
+
+class CustomException(Exception):
+    pass
+
+
+try:
+    raise CustomException("This is a userdefined exception demo!!!")
+except CustomException as errormessage:
+    print(errormessage)
+
+
