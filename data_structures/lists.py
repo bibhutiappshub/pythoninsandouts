c6cc892f446609740b6c18f7ae8dc5d5bdfee2cf
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2023-10-19 10:56:58 +0530

    Lists operations

--- data_structures/lists.py
+++ data_structures/lists.py
@@ -73,6 +73,29 @@ print(list_copy1)
 print(list_copy2)
 print(list_copy3)
 
+# List compressions are preferred over loops as the operation can be
+# done using the single line.
+
+# Suppose we want to make a new list by with power of 2 ofthe following list.
+# Then we can do it 2 ways. By using loops or by list compression.
+
+num_list = [2, 3, 4, 5]
+new_num_list = []
+
+# Method-1
+for num in num_list:
+    new_num_list.append(num**2)
+
+print(new_num_list)
+
+# Method-2
+compressed_list = [num ** 3 for num in num_list]
+print(compressed_list)
+
+# Method-3
+mapped_list = list(map(lambda n: n**4, num_list))
+print(mapped_list)
+
 # Clear all the elements from the list
 my_list.clear()
 print(my_list)
